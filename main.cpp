5a5bb8cb1d5380ec9289ccbfa5574579ab71db09
unknown <nrkemp@SY-T2113-15.SeneDS.senecacollege.ca>
2015-06-19 10:49:29 -0400

    Did some work...not much... but work

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define NUM_STRINGS	6
+#define NUM_FRETS	20
+
+typedef struct {
+	char _data[NUM_STRINGS][NUM_FRETS][6];
+} fretboard_t;
+
+char fretboard_data[NUM_STRINGS][NUM_FRETS][6] = {	{ "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" },
+													{ "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E" },
+													{ "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" },
+													{ "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D" },
+													{ "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb" },
+													{ "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" }
+												};
+
+fretboard_t fretboard;
+
+void Print() {
+	int x, y;
+	
+	for (x = 0; x < NUM_STRINGS; x++) {
+		for (y = 0; y < NUM_FRETS; y++)
+			printf("|\t%s\t", fretboard_data[x][y]);
+			printf("|\n");
+	}
+}
+
+int main() {
+	Print();
+	return 0;
+}
